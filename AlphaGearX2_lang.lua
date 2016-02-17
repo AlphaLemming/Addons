388458d40c08cb5c3737362f6a3d65807fc76dd8
AlphaLemming <menni_privat@gmx.de>
2016-02-17 08:54:35 +0100

    Create AlphaGearX2_lang.lua

---
+++ AlphaGearX2_lang.lua
@@ -0,0 +1,17 @@
+options = {
+	'UI-Button anzeigen',				-- 1
+	'UI-Set-Button anzeigen',			-- 2
+	'Reparatur-Icon anzeigen',			-- 3
+	'Reparatur-Kosten anzeigen',		-- 4
+	'Waffen-Ladung-Icon(s) anzeigen',	-- 5
+	'Waffenwechsel-Meldung anzeigen',	-- 6
+	'Alle Interface-Elemente sperren',	-- 7
+	'-',
+	'Set-Items im Inventar markieren',	-- 9
+	'Item-Zustand in Prozent anzeigen',	-- 10
+	'Item-Qualität als Farbe anzeigen',	-- 11
+	'-',
+	'Fenster bei Bewegung schließen',	-- 13
+	'-',
+	'Waffen automatisch aufladen',		-- 15
+}
