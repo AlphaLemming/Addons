0c36a301c0b03773e24dad24e6a320224bf46b66
AlphaLemming <menni_privat@gmx.de>
2016-02-22 11:24:53 +0100

    Update AlphaGearX2_lang.lua

--- AlphaGearX2_lang.lua
+++ AlphaGearX2_lang.lua
@@ -29,24 +29,25 @@ de = {
 		Gear = lmb..' Gesamte Ausrüstung anlegen\n'..rmb..' Weitere Optionen',
 		Skill = lmb..' Alle Fähigkeiten ausrüsten\n'..mmb..' Weitere Optionen'
 	},
+	OptionWidth = 310,
 	Options = {
 		SHOW_BUTTON			= 'UI-Button anzeigen',				-- 1
 		SHOW_SET_BUTTONS	= 'UI-Set-Buttons anzeigen',			-- 2
 		SHOW_REPAIR_ICON	= 'Reparatur-Icon anzeigen',			-- 3
 		SHOW_REPAIR_COST	= 'Reparatur-Kosten anzeigen',		-- 4
 		SHOW_CHARGE_STATUS	= 'Waffen-Ladung-Icon(s) anzeigen',	-- 5
-		'-',
+		ROW1 = '-',
 		SHOW_SET_SWAP		= 'Set-Wechsel-Meldung anzeigen',	-- 6
 		SHOW_BAR_SWAR		= 'Waffenwechsel-Meldung anzeigen',
 		SHOW_EQUIPPED_SET	= 'Angelegtes Set anzeigen',
-		'-',
+		ROW2 = '-',
 		MARK_INVENTORY		= 'Set-Items im Inventar markieren',	-- 9
 		SHOW_CONDITION		= 'Item-Zustand in Prozent anzeigen',	-- 10
 		SHOW_QUALITY		= 'Item-Qualität als Farbe anzeigen',	-- 11
-		'-',
+		ROW3 = '-',
 		MOVEMENT_CLOSE		= 'Fenster bei Bewegung schließen',	-- 13
 		LOCK_INTERFACE		= 'Alle AlphaGear-Elemente sperren',	-- 7
-		'-',
+		ROW4 = '-',
 		AUTO_CHARGE			= 'Waffen automatisch aufladen',		-- 15
 	}
 },
@@ -61,7 +62,7 @@ en = {
 	NotEnoughSpace = '|cFFAA33AlphaGear|r |cFF0000Not enough bag-space...|r',
 	SoulgemUsed = '<<C:1>> |cFFAA33was recharged.|r',
 	SetPart = 'Part of Set: <<C:1>>',
-	Lock = 'Ist das Set gesperrt, werden leere Plätze ausgezogen.\nIst das Set entsperrt, werden leere Plätze ignoriert.\n\n'..lmb..' Sperren/Entsperren',
+	Lock = 'If the set is locked, all empty slots will be unequipped.\nIf the set is unlocked, all empty slots will be ignored.\n\n'..lmb..' Lock/unlock',
 	SetConnector = {
 		lmb..' Connect gear with set\n'..rmb..' Remove connection',
 		lmb..' Connect actionbar 1 with set\n'..rmb..' Remove connection',
@@ -79,24 +80,25 @@ en = {
 		Gear = lmb..' Equip entire gear\n'..rmb..' More options',
 		Skill = lmb..' Equip all skills\n'..mmb..' More options'
 	},
+	OptionWidth = 300,
 	Options = {
 		SHOW_BUTTON			= 'Show UI-button',
 		SHOW_SET_BUTTONS	= 'Show UI-set-buttons',
 		SHOW_REPAIR_ICON	= 'Show repair icon',
 		SHOW_REPAIR_COST	= 'Show repair cost',
 		SHOW_CHARGE_STATUS	= 'Show weapon charge icon(s)',
-		'-',
+		ROW1 = '-',
 		SHOW_SET_SWAP		= 'Show set equip message',
 		SHOW_BAR_SWAR		= 'Show weapon swap message',
 		SHOW_EQUIPPED_SET	= 'Show equipped set',
-		'-',
+		ROW2 = '-',
 		MARK_INVENTORY		= 'Mark set items in inventory',
 		SHOW_CONDITION		= 'Show item condition in percent',
 		SHOW_QUALITY		= 'Show item quality as color',
-		'-',
+		ROW3 = '-',
 		MOVEMENT_CLOSE		= 'Close window on movement',
 		LOCK_INTERFACE		= 'Lock all AlphaGear elements',
-		'-',
+		ROW4 = '-',
 		AUTO_CHARGE			= 'Automatic weapon charge'
 	}
 },
@@ -108,7 +110,7 @@ fr = {
 	Icon = lmb..'Sélectionnez l\'icône',
 	Set = lmb..' Équiper l\'ensemble\n'..rmb..' Modifier l\'ensemble',
 	NotFound = '<<1>> |cFF0000n\'a pas été trouvé...|r',
-	NotEnoughSpace = '|cFFAA33AlphaGear|r |cFF0000Not enough bag-space...|r',
+	NotEnoughSpace = '|cFFAA33AlphaGear|r |cFF0000Pas assez d\'espace d\'inventaire...|r',
 	SoulgemUsed = '<<C:1>> |cFFAA33a été rechargé.|r',
 	SetPart = 'Élément de l\'ensemble: <<C:1>>',
 	Lock = 'Si l\'ensemble est verrouillé, tous les slots vides seront déséquipés.\nSi l\'ensemble est déverrouillé, tous les slots vides seront ignorés.\n\n'..lmb..' Verrouiller/Déverrouiller',
@@ -129,24 +131,25 @@ fr = {
 		Gear = lmb..' Équiper tout l\'équipement\n'..rmb..' plus d\'options',
 		Skill = lmb..' Placer toutes les compétences\n'..mmb..' plus d\'options'
 	},
+	OptionWidth = 400,
 	Options = {
 		SHOW_BUTTON			= 'Afficher le bouton de l\'interface',
 		SHOW_SET_BUTTONS	= 'Afficher les boutons d\'ensembles',
 		SHOW_REPAIR_ICON	= 'Afficher l\'icône de réparation',
 		SHOW_REPAIR_COST	= 'Afficher le coup de réparation',
 		SHOW_CHARGE_STATUS	= 'Afficher les icônes de charge d\'arme',
-		'-',
+		ROW1 = '-',
 		SHOW_SET_SWAP		= 'Afficher le message de changement d\'équipement',
 		SHOW_BAR_SWAR		= 'Afficher le message de switch d\'arme',
 		SHOW_EQUIPPED_SET	= 'Afficher l\'ensemble porté',
-		'-',
+		ROW2 = '-',
 		MARK_INVENTORY		= 'Marquer les objets de set dans l\'inventaire',
 		SHOW_CONDITION		= 'Afficher le taux d\'usure en pourcentage',
 		SHOW_QUALITY		= 'Afficher la qualité de l\'objet en tant que couleur',
-		'-',
-		MOVEMENT_CLOSE		= 'Fermer la fenêtre au déplacement du personnage',	-- 13
-		LOCK_INTERFACE		= 'Verrouiller les éléments AlphaGear',-- 7
-		'-',
+		ROW3 = '-',
+		MOVEMENT_CLOSE		= 'Fermer la fenêtre au déplacement du personnage',
+		LOCK_INTERFACE		= 'Verrouiller les éléments AlphaGear',
+		ROW4 = '-',
 		AUTO_CHARGE			= 'Rechargement automatique de l\'arme'
 	}
 }
