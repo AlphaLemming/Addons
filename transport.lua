7c4062c6777152e3e7639a98c95da531066687b1
AlphaLemming <menni_privat@gmx.de>
2016-04-20 17:48:28 +0200

    Update transport.lua

--- transport.lua
+++ transport.lua
@@ -1,46 +1,55 @@
-style = {{CRAFTING_TYPE_CLOTHIER,1},{CRAFTING_TYPE_CLOTHIER,8},{CRAFTING_TYPE_BLACKSMITHING,8}},
-	
-	local function SetPlayerResearch(craft,data,known,unknown)
-		local p, current, color, maxsim, level, rank, c, n = WM:GetControlByName('CS4_Research'..craft), 0, '|cFFFFFF'
-		local pip = '|r|c808080 '..GetString(SI_BULLET)..'|r '
-		for x = 1,3 do
-			c = p:GetNamedChild('Line'..x)
-			n = c:GetNamedChild('Name')
-			if data[x] then
-				c:GetNamedChild('Time'):SetText(TOOLS:GetTime(data[x][3]))
-				n:SetText(data[x][1])
-				n.cs_data = { info = data[x][2], anchor = {3,n,9,0,2} }
-				current = current + 1
-			else
-				c:GetNamedChild('Time'):SetText('')					
-				n:SetText('')
-				n.cs_data = nil
-			end
-		end
-		rank, level, maxsim = unpack(CraftStore.account.player[CHAR].crafting[craft])
-		if maxsim > 1 then if current == maxsim then color = '|c00FF00' else color = '|cFF0000' end end
-		p:GetNamedChild('Data'):SetText('|t25:25:'..CraftStore.crafticon[craft]..'|t |c00FF00'..known..pip..'|cFF0000'..unknown..pip..'|c808080'..LANG:Get('level')..': '..level..' ('..rank..')|r')
-		p:GetNamedChild('Slot'):SetText(color..current..' / '..maxsim..'|r')
-	end
+<Button name="$(parent)24Hour" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
+	<Anchor point="12" relativePoint="6" relativeTo="$(parent)Research6" offsetX="-5" />
+	<Dimensions x="72" y="41" />
+	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+</Button>
+<Button name="$(parent)12Hour" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
+	<Anchor point="8" relativePoint="2" relativeTo="$(parent)24Hour" offsetX="-5" />
+	<Dimensions x="72" y="41" />
+	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+</Button>
+<Button name="$(parent)Player" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
+	<Anchor point="8" relativePoint="2" relativeTo="$(parent)12Hour" offsetX="-5" />
+	<Dimensions x="205" y="41" />
+	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+</Button>
+<Button name="$(parent)Menu" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
+	<Anchor point="8" relativePoint="2" relativeTo="$(parent)Player" offsetX="-5" />
+	<Dimensions x="153" y="31" />
+	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+</Button>
+<Button name="$(parent)Sign1" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
+	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
+	<Dimensions x="153" y="31" />
+	<Textures normal="CraftStore4/grey.dds" />
+	<FontColors normalColor="999999" />
+</Button>
+<Button name="$(parent)Sign2" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
+	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
+	<Dimensions x="153" y="301" />
+	<Textures normal="CraftStore4/grey.dds" />
+	<FontColors normalColor="999999" />
+</Button>
+<Button name="$(parent)Sign3" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
+	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
+	<Dimensions x="153" y="337" />
+	<Textures normal="CraftStore4/grey.dds" />
+	<FontColors normalColor="999999" />
+</Button>
+<Button name="$(parent)Sign4" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
+	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
+	<Dimensions x="153" y="603" />
+	<Textures normal="CraftStore4/grey.dds" />
+	<FontColors normalColor="999999" />
+</Button>
 
-	function self:GetPlayerResearch()
-		local crafting = {CRAFTING_TYPE_BLACKSMITHING, CRAFTING_TYPE_CLOTHIER, CRAFTING_TYPE_WOODWORKING}
-		for _,craft in pairs(crafting) do
-			local research, known, unknown = {}, 0, 0
-			for line = 1, GetNumSmithingResearchLines(craft) do
-				for trait = 1, maxtraits do
-					local val = CraftStore.account.player[CHAR].research[craft][line][trait] or false
-					if val == true then
-						known = known + 1
-					elseif val == false then
-						unknown = unknown + 1
-					elseif val > 0 then
-						table.insert(research,{'|t23:23:'..icon..'|t '..name,desc,dur})
-					end
-					SetIcon(craft,line,trait)
-				end
-				SetCount(craft,line)
-			end
-			SetPlayerResearch(craft,research,known,unknown)
-		end
-	end
+CS4_PanelMenu:SetText('t16:16:CraftStore4/menu.dds|t '..LANG:Get('menu'))
+CS4_PanelSign1:SetText(GetString()..'|t5:5:x.dds|t')
+CS4_PanelSign2:SetText(GetString()..'|t5:5:x.dds|t')
+CS4_PanelSign3:SetText(GetString()..'|t5:5:x.dds|t')
+CS4_PanelSign4:SetText(GetString()..'|t5:5:x.dds|t')
+
