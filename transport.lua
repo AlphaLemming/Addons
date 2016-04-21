30e82638242d8632205630683af1150954b44758
AlphaLemming <menni_privat@gmx.de>
2016-04-21 17:57:15 +0200

    Update transport.lua

--- transport.lua
+++ transport.lua
@@ -1,55 +1,53 @@
-<Button name="$(parent)24Hour" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
-	<Anchor point="12" relativePoint="6" relativeTo="$(parent)Research6" offsetX="-5" />
-	<Dimensions x="72" y="41" />
-	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-</Button>
-<Button name="$(parent)12Hour" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
-	<Anchor point="8" relativePoint="2" relativeTo="$(parent)24Hour" offsetX="-5" />
-	<Dimensions x="72" y="41" />
-	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-</Button>
-<Button name="$(parent)Player" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
-	<Anchor point="8" relativePoint="2" relativeTo="$(parent)12Hour" offsetX="-5" />
-	<Dimensions x="205" y="41" />
-	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-</Button>
-<Button name="$(parent)Menu" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold" clickSound="Click">
-	<Anchor point="8" relativePoint="2" relativeTo="$(parent)Player" offsetX="-5" />
-	<Dimensions x="153" y="31" />
-	<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-	<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-</Button>
-<Button name="$(parent)Sign1" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
-	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
-	<Dimensions x="153" y="31" />
-	<Textures normal="CraftStore4/grey.dds" />
-	<FontColors normalColor="999999" />
-</Button>
-<Button name="$(parent)Sign2" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
-	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
-	<Dimensions x="153" y="301" />
-	<Textures normal="CraftStore4/grey.dds" />
-	<FontColors normalColor="999999" />
-</Button>
-<Button name="$(parent)Sign3" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
-	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
-	<Dimensions x="153" y="337" />
-	<Textures normal="CraftStore4/grey.dds" />
-	<FontColors normalColor="999999" />
-</Button>
-<Button name="$(parent)Sign4" verticalAlignment="1" horizontalAlignment="2" font="CS4Font">
-	<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
-	<Dimensions x="153" y="603" />
-	<Textures normal="CraftStore4/grey.dds" />
-	<FontColors normalColor="999999" />
-</Button>
-
-CS4_PanelMenu:SetText('t16:16:CraftStore4/menu.dds|t '..LANG:Get('menu'))
-CS4_PanelSign1:SetText(GetString()..'|t5:5:x.dds|t')
-CS4_PanelSign2:SetText(GetString()..'|t5:5:x.dds|t')
-CS4_PanelSign3:SetText(GetString()..'|t5:5:x.dds|t')
-CS4_PanelSign4:SetText(GetString()..'|t5:5:x.dds|t')
-
+		<TopLevelControl hidden="true" mouseEnabled="true" name="CS4_Commander" topmost="true">
+			<Anchor point="128" relativePoint="128" relativeTo="GuiRoot" />
+			<Dimensions x="350" y="350" />
+			<Controls>
+				<Texture name="$(parent)BG"><AnchorFill /></Texture>
+				<Texture name="$(parent)Logo">
+					<Anchor point="128" relativePoint="1" relativeTo="$(parent)" />
+					<Dimensions y="128" x="128" />
+				</Texture>
+				<Button name="$(parent)Button1" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="1" relativeTo="$(parent)" offsetY="100" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_Panel)</OnClicked>
+				</Button>
+				<Button name="$(parent)Button2" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parentButton1" offsetY="5" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_Style)</OnClicked>
+				</Button>
+				<Button name="$(parent)Button3" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parentButton2" offsetY="5" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_SetCraft)</OnClicked>
+				</Button>
+				<Button name="$(parent)Button4" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parentButton3" offsetY="5" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_Cook)</OnClicked>
+				</Button>
+				<Button name="$(parent)Button5" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parentButton4" offsetY="5" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_Rune)</OnClicked>
+				</Button>
+				<Button name="$(parent)Button6" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Bold">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parentButton5" offsetY="5" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+					<Dimensions y="300" x="40" />
+					<OnClicked>SCENE_MANAGER:ToggleTopLevel(CS4_Flask)</OnClicked>
+				</Button>
+			</Controls>
+		</TopLevelControl>
