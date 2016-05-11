b77375c610de553ef8fb56060e3460cf6ffd007c
AlphaLemming <menni_privat@gmx.de>
2016-05-11 17:40:30 +0200

    Update transport.lua

--- transport.lua
+++ transport.lua
@@ -1,71 +1,80 @@
-function self:NilZero(val)
-	if not val or val == 0 then return nil end
-	return val
-end
-
-	EM:RegisterForEvent('CraftStore_PlayerLoad',EVENT_PLAYER_ACTIVATED,function()
-		if CraftStore.init then return end
-	end)
-
-
-			bag = {nil,'Bank',nil,nil,'Handwerkstasche',nil}
-
-
-		local function HandleItem(_,bag,slot,new)
-			if bag ~= BAG_VIRTUAL and bag ~= BAG_BACKPACK and bag ~= BAG_BANK then return end
-			local stock = CraftStore.account.stock
-			local link = GetItemLink(bag,slot) or bagCache[bag][slot].link
-			local uid = Id64ToString(GetItemUniqueId(bag,slot)) or bagCache[bag][slot].uid
-			local id = TOOLS:SplitLink(link,3) or bagCache[bag][slot].id
-			local a1, a2, a3 = GetItemLinkStacks(link)
-			if new then bagCache[bag][slot] = { link = link, uid = uid, id = id }
-			else bagCache[bag][slot] = nil end
-			if not stock[id] then stock[id] = {} end
-			stock[id].link = link
-			stock[id][SELF] = TOOLS:NilZero(a1)
-			stock[id]['aa'] = TOOLS:NilZero(a2)
-			stock[id]['ab'] = TOOLS:NilZero(a3)
-			if TOOLS:TabLen(stock[id]) == 1 then stock[id] = nil end
-			local craft,line,trait = TRAIT:FindTrait(link)
-			if craft and line and trait and uid then
-				local store = CraftStore.account.stored[craft..'&'..line..'&'..trait] or { link = false, id = 0 }
-				local owner = LANG:Get('bag')[bag] or SELF
-				if uid == store.id then
-					if not GetItemInstanceId(bag,slot) then store[craft..'&'..line..'&'..trait] = nil
-					else store[craft..'&'..line..'&'..trait].owner = owner end
-				elseif TOOLS:CompareItem(link, store.link) and not GetItemLinkSetInfo(link) then
-					store[craft..'&'..line..'&'..trait] = { link = link, owner = owner, id = uid }
-				end
-				SetIcon(craft,line,trait)
-			end
-		end
-		
-		for _,bag in pairs(bags) do
-			bagCache[bag] = {}
-			for slot = 0, GetBagSize(bag)-1 do
-				local link, uid, id = GetItemLink(bag,slot), Id64ToString(GetItemUniqueId(bag,slot))
-				id = TOOLS:SplitLink(link,3)
-				bagCache[bag][slot] = { link = link, uid = uid, id = id }
-				local a1, a2, a3 = GetItemLinkStacks(link)
-				if not stock[id] then stock[id] = {} end
-				stock[id].link = link
-				stock[id][SELF] = TOOLS:NilZero(a1)
-				stock[id]['aa'] = TOOLS:NilZero(a2)
-				stock[id]['ab'] = TOOLS:NilZero(a3)
-				if TOOLS:TabLen(stock[id]) == 1 then stock[id] = nil end
-				local craft,line,trait = TRAIT:FindTrait(link)
-				if craft and line and trait and uid then
-					local store = CraftStore.account.stored[craft..'&'..line..'&'..trait] or { link = false, id = 0 }
-					local owner = LANG:Get('bag')[bag] or SELF
-					if uid == store.id then
-						if not GetItemInstanceId(bag,slot) then store[craft..'&'..line..'&'..trait] = nil
-						else store[craft..'&'..line..'&'..trait].owner = owner end
-					elseif TOOLS:CompareItem(link, store.link) and not GetItemLinkSetInfo(link) then
-						store[craft..'&'..line..'&'..trait] = { link = link, owner = owner, id = uid }
-					end
-				end
-			end
-		end
-
-		EM:RegisterForEvent('CraftStore_HandleItem', EVENT_INVENTORY_SINGLE_SLOT_UPDATE, HandleItem)
-		EM:RegisterForEvent('CraftStore_SkillChanged', EVENT_SKILL_POINTS_CHANGED, SetPlayerSkill)
+		<TopLevelControl hidden="true" clampedToScreen="true" movable="true" mouseEnabled="true" name="CS4_SetCraft" allowBringToTop="true">
+			<Dimensions x="501" y="587" />
+			<OnMouseUp>CraftStore.account.position[3] = {self:GetLeft(),self:GetTop()}</OnMouseUp>
+			<Controls>
+				<Backdrop name="$(parent)BG" centerColor="181818" edgeColor="00000000">
+					<AnchorFill/>
+					<Edge edgeSize="1" />
+				</Backdrop>
+				<Label name="$(parent)Headline" font="CS4Font" color="FFAA33">
+					<Anchor point="1" relativePoint="1" relativeTo="$(parent)" offsetY="5" />
+				</Label>
+				<Button name="$(parent)CloseButton" clickSound="Click">
+					<Anchor point="9" relativePoint="9" relativeTo="$(parent)" offsetX="-7" offsetY="4" />
+					<Dimensions y="25" x="25" />
+					<Textures normal="esoui/art/buttons/decline_up.dds" pressed="esoui/art/buttons/decline_down.dds" mouseOver="esoui/art/buttons/decline_over.dds" />
+					<OnClicked>SCENE_MANAGER:HideTopLevel(CS4_SetCraft)</OnClicked>
+				</Button>
+				<Button name="$(parent)Header" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
+					<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
+					<Dimensions x="481" y="40" />
+					<Textures normal="CraftStore4/grey.dds" />
+					<FontColors normalColor="FFAA33" mouseOverColor="FFAA33" />
+				</Button>
+				<Button name="$(parent)Player" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
+					<Anchor point="3" relativePoint="6" relativeTo="$(parent)Header" offsetY="1" />
+					<Dimensions x="230" y="32" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+				</Button>
+				<Control name="$(parent)Traitinfo">
+					<Anchor point="3" relativePoint="6" relativeTo="$(parent)Player" offsetY="1" />
+					<Dimensions x="230" y="164" />
+				</Control>
+				<Backdrop name="$(parent)List" centerColor="383838" edgeColor="00383838" inherits="ZO_ScrollContainerBase">
+					<Anchor point="9" relativePoint="12" relativeTo="$(parent)Header" offsetY="1" />
+					<Dimensions x="250" y="350" />
+					<Edge edgeSize="1" />
+					<OnInitialized>ZO_Scroll_Initialize(self)</OnInitialized>
+					<Controls>
+						<Control name="$(parent)ScrollChild">
+							<OnInitialized>self:SetParent(self:GetParent():GetNamedChild("Scroll"));self:SetAnchor(3,nil,3,0,0)</OnInitialized>
+						</Control>
+					</Controls>
+				</Backdrop>
+				<Backdrop name="$(parent)Info" centerColor="383838" edgeColor="00383838" inherits="ZO_ScrollContainerBase">
+					<Anchor point="9" relativePoint="12" relativeTo="$(parent)List" offsetY="1" />
+					<Dimensions x="481" y="150" />
+					<Edge edgeSize="1" />
+					<OnInitialized>ZO_Scroll_Initialize(self)</OnInitialized>
+					<Controls>
+						<Control name="$(parent)ScrollChild">
+							<OnInitialized>self:SetParent(self:GetParent():GetNamedChild("Scroll"));self:SetAnchor(3,nil,3,0,0)</OnInitialized>
+							<Controls>
+								<Label name="$(parent)Text" font="CS4Font" color="FFFFFF">
+									<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="10" />
+								</Label>
+							</Controls>
+						</Control>
+					</Controls>
+				</Backdrop>
+				<Button name="$(parent)Shrine1" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Traitinfo" offsetY="1" />
+					<Dimensions x="230" y="50" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+				</Button>
+				<Button name="$(parent)Shrine2" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Shrine1" offsetY="1" />
+					<Dimensions x="230" y="50" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+				</Button>
+				<Button name="$(parent)Shrine3" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
+					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Shrine2" offsetY="1" />
+					<Dimensions x="230" y="50" />
+					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
+					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
+				</Button>
+			</Controls>
+		</TopLevelControl>
