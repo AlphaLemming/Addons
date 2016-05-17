dd5171d5b75ad8bf0ac76ee546ffc49a4dbfdeb7
AlphaLemming <menni_privat@gmx.de>
2016-05-17 17:52:55 +0200

    Update transport.lua

--- transport.lua
+++ transport.lua
@@ -1,80 +1,31 @@
-		<TopLevelControl hidden="true" clampedToScreen="true" movable="true" mouseEnabled="true" name="CS4_SetCraft" allowBringToTop="true">
-			<Dimensions x="501" y="587" />
-			<OnMouseUp>CraftStore.account.position[3] = {self:GetLeft(),self:GetTop()}</OnMouseUp>
-			<Controls>
-				<Backdrop name="$(parent)BG" centerColor="181818" edgeColor="00000000">
-					<AnchorFill/>
-					<Edge edgeSize="1" />
-				</Backdrop>
-				<Label name="$(parent)Headline" font="CS4Font" color="FFAA33">
-					<Anchor point="1" relativePoint="1" relativeTo="$(parent)" offsetY="5" />
-				</Label>
-				<Button name="$(parent)CloseButton" clickSound="Click">
-					<Anchor point="9" relativePoint="9" relativeTo="$(parent)" offsetX="-7" offsetY="4" />
-					<Dimensions y="25" x="25" />
-					<Textures normal="esoui/art/buttons/decline_up.dds" pressed="esoui/art/buttons/decline_down.dds" mouseOver="esoui/art/buttons/decline_over.dds" />
-					<OnClicked>SCENE_MANAGER:HideTopLevel(CS4_SetCraft)</OnClicked>
-				</Button>
-				<Button name="$(parent)Header" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
-					<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="35" />
-					<Dimensions x="481" y="40" />
-					<Textures normal="CraftStore4/grey.dds" />
-					<FontColors normalColor="FFAA33" mouseOverColor="FFAA33" />
-				</Button>
-				<Button name="$(parent)Player" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
-					<Anchor point="3" relativePoint="6" relativeTo="$(parent)Header" offsetY="1" />
-					<Dimensions x="230" y="32" />
-					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-				</Button>
-				<Control name="$(parent)Traitinfo">
-					<Anchor point="3" relativePoint="6" relativeTo="$(parent)Player" offsetY="1" />
-					<Dimensions x="230" y="164" />
-				</Control>
-				<Backdrop name="$(parent)List" centerColor="383838" edgeColor="00383838" inherits="ZO_ScrollContainerBase">
-					<Anchor point="9" relativePoint="12" relativeTo="$(parent)Header" offsetY="1" />
-					<Dimensions x="250" y="350" />
-					<Edge edgeSize="1" />
-					<OnInitialized>ZO_Scroll_Initialize(self)</OnInitialized>
-					<Controls>
-						<Control name="$(parent)ScrollChild">
-							<OnInitialized>self:SetParent(self:GetParent():GetNamedChild("Scroll"));self:SetAnchor(3,nil,3,0,0)</OnInitialized>
-						</Control>
-					</Controls>
-				</Backdrop>
-				<Backdrop name="$(parent)Info" centerColor="383838" edgeColor="00383838" inherits="ZO_ScrollContainerBase">
-					<Anchor point="9" relativePoint="12" relativeTo="$(parent)List" offsetY="1" />
-					<Dimensions x="481" y="150" />
-					<Edge edgeSize="1" />
-					<OnInitialized>ZO_Scroll_Initialize(self)</OnInitialized>
-					<Controls>
-						<Control name="$(parent)ScrollChild">
-							<OnInitialized>self:SetParent(self:GetParent():GetNamedChild("Scroll"));self:SetAnchor(3,nil,3,0,0)</OnInitialized>
-							<Controls>
-								<Label name="$(parent)Text" font="CS4Font" color="FFFFFF">
-									<Anchor point="3" relativePoint="3" relativeTo="$(parent)" offsetX="10" offsetY="10" />
-								</Label>
-							</Controls>
-						</Control>
-					</Controls>
-				</Backdrop>
-				<Button name="$(parent)Shrine1" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
-					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Traitinfo" offsetY="1" />
-					<Dimensions x="230" y="50" />
-					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-				</Button>
-				<Button name="$(parent)Shrine2" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
-					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Shrine1" offsetY="1" />
-					<Dimensions x="230" y="50" />
-					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-				</Button>
-				<Button name="$(parent)Shrine3" clickSound="Click" verticalAlignment="1" horizontalAlignment="1" font="CS4Font">
-					<Anchor point="1" relativePoint="4" relativeTo="$(parent)Shrine2" offsetY="1" />
-					<Dimensions x="230" y="50" />
-					<Textures normal="CraftStore4/grey.dds" mouseOver="CraftStore4/over.dds" />
-					<FontColors normalColor="FFFFFF" mouseOverColor="FFAA33" />
-				</Button>
-			</Controls>
-		</TopLevelControl>
+local crafting = {CRAFTING_TYPE_BLACKSMITHING, CRAFTING_TYPE_CLOTHIER, CRAFTING_TYPE_WOODWORKING}
+local xpos, ypos = 0, 0
+for _,craft in pairs(crafting) do
+	for line = 1, GetNumSmithingResearchLines(craft) do
+		local craftname, name, icon = TOOLS:GetLineInfo(craft,line)
+		local c = WM:CreateControl('$(parent)Icon'..craft..'&'..line,CS4_SetCraftTraitinfo,CT_BUTTON)
+		c:SetAnchor(3,CS4_SetCraftTraitinfo,3,xpos,ypos)
+		c:SetDimensions(32,32)
+		c:SetNormalTexture('CraftStore4/grey.dds')
+		c:SetMouseOverTexture('CraftStore4/over.dds')
+		c.cs_data = { info = '|cFFFFFF'..craftname..'|r\n'..name..' ('..self.cs_data.traits..')', anchor = {c,1,4,0,2}, traits = 0 }
+		local t = WM:CreateControl('$(parent)Texture',c,CT_TEXTURE)
+		t:SetAnchor(128,c,128,0,0)
+		t:SetDimensions(28,28)
+		t:SetTexture(icon)
+		xpos = xpos + 33
+		if xpos == 231 then xpos = 0; ypos = ypos + 33 end
+	end
+end
+
+function UpdateSet(nr)
+	for _,craft in pairs(crafting) do
+		for line = 1, GetNumSmithingResearchLines(craft) do
+			local c = WM:GetControlByName('CS4_SetCraftTraitinfoIcon'..craft..'&'..line)
+			local t, count = c:GetNamedChild('Texture'), GetTraitCount(craft,line)
+			if count >= CraftStore.sets[nr].traits then t:SetColor(1,1,1,1) else t:SetColor(1,0,0,1) end
+			c.cs_data.traits = count..' / '..CraftStore.sets[nr].traits
+		end
+	end
+	CS4_SetCraftHeader:SetText(CraftStore.sets[nr].item)
+end
